8122614479d77317a1fbc07000f8323e339bee57
Dastar Ron <dastar.ron@gmail.com>
2020-04-18 22:47:58 +0300

    abstract/info

---
+++ Abstract/Info.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Abstract
+{
+    public class Info
+    {
+        protected Info()
+        {
+        }
+
+        public void SetValue(string name, object value)
+        {
+            var field = GetType().GetField(name);
+            if (field is null)
+            {
+                throw new Exception($"Can't find field {name}");
+            }
+
+            try
+            {
+                var safeValue = Convert.ChangeType(value, field.FieldType);
+                field.SetValue(this, safeValue);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Can't set value to field {name}", ex);
+            }
+        }
+    }
+}
---
+++ SheepGame/MapInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Abstract;
+
+namespace SheepGame.Desktop
+{
+    public class MapInfo: Info
+    {
+        public string Name;
+        public int MapWidth;
+        public int MapHeight;
+        public Asset Tile;
+        public string ImgDir;
+
+        public MapInfo()
+        {
+        }
+    }
+}
